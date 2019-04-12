e6593a4a9b62a00df611f4e163333bfd5b4b1a49
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2019-04-11 20:00:04 -0400

    Add files via upload

---
+++ BlueSocket Button.user.js
@@ -0,0 +1,50 @@
+// ==UserScript==
+// @name         BlueSocket Button
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  Opens sites corresponding BlueSocket controller.
+// @author       Zachary Kitcher
+// @match        https://vselect.com/*?goto=bblancircuits&locid=*
+// ==/UserScript==
+
+
+(function() {
+
+    var btn = document.createElement("BUTTON");
+    var Space = document.createElement("TD");
+
+    var anchor = document.getElementsByClassName("bbutton")[8].parentNode;
+    var parent = anchor.parentNode;
+
+    var indexLocGroup = document.getElementsByName("locgroup")[0].selectedIndex;
+    var optionLocGroup = document.getElementsByName("locgroup")[0].options;
+    var locGroupString = optionLocGroup[indexLocGroup].text;
+    var cheetahTest = "Cheetah";
+
+    var BSSelection, BSString, optionBSSelection, indexBSSelection
+
+    btn.innerHTML = "Bluesocket Controller";
+    btn.className = "bbutton";
+    btn.style.width = '150px';
+    Space.style.width = '3px';
+
+    if (locGroupString.toLowerCase().indexOf(cheetahTest.toLowerCase()) != -1 ) {
+        parent.insertBefore(Space, anchor);
+        parent.insertBefore(btn, Space);
+    }
+
+    btn.addEventListener ("click", function() {
+        BSSelection = document.getElementsByClassName("vselect-dropdown")[0];
+        if (BSSelection.value > 0){
+            indexBSSelection = document.getElementsByClassName("vselect-dropdown")[0].selectedIndex;
+            optionBSSelection = document.getElementsByClassName("vselect-dropdown")[0].options;
+            BSString = optionBSSelection[indexBSSelection].text;
+            BSString = BSString.replace(/\D+/g, "");
+            window.open("https://162.220.74." + BSString + ":3000/");
+        }
+        else
+        {
+            window.open("https://i.imgflip.com/2ybxhk.jpg");
+        };
+    });
+})();
